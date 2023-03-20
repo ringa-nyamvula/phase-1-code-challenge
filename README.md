8aa55c5b9ad0f47237d4728ec7c6a5ade7c1c257
nyamvularinga <nyamvularinga@gmail.com>
2023-03-20 11:58:57 +0300

    Complete Assignment

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# phase-1-code-challenge
+# Phase 1 Code Challenge
+## Goals for the challenge
+    1. To test and see if i can apply the skills learnt so far
+    2. to further practice on the content learnt through phase 1 and phase 0
+
+## Questions to tackle 
+    Challenge 1: Student Grade Generator (Toy Problem)
+    Write that prompts the user to input student marks. The input should be between 0 and 100. Then output the correct grade: 
+
+    A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40.
+
+ 
+
+    Challenge 2: Speed Detector (Toy Problem)
+    Write a program that takes as input the speed of a car e.g 80. If the speed is less than 70, it should print “Ok”. Otherwise, for every 5 km/s above the speed limit (70), it should give the driver one demerit point and print the total number of demerit points.
+
+    For example, if the speed is 80, it should print: “Points: 2”. If the driver gets more than 12 points, the function should print: “License suspended”.
+
+ 
+
+    Challenge 3: Net Salary Calculator (Toy Problem)
+    Write a program whose major task is to calculate an individual’s Net Salary by getting the inputs of basic salary and benefits. Calculate the payee (i.e. Tax), NHIFDeductions, NSSFDeductions, gross salary, and net salary. 
+
+## Approaches used 
+    Mainly used functions to create a working programforthe threee challenges which can be recalled when one need to execute a computation depending on what the computation is .
+    There are three functions: 
+        1.function gradeSystem
+        2.function speedCheck
+        3.function calcSalary (values used here are from ):
+                https://www.aren.co.ke/payroll/taxrates.
+                https://www.kra.go.ke/en/individual/calculate-tax/calculating-tax/paye
+
+
+
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+// A function for computing grades for students
+// The program first checks the score entered and grades it according to the grade limits set for each grade
 function gradeSystem(event)
 {
-    const score = parseInt(document.getElementById('gradeSystem').value)
+    let score;
 if (score >79 && score <=100)
 {
     return "A"
@@ -23,15 +25,20 @@ else if (score < 40 && score <= 0)
 }
 }
 
+// A function for computing cars speedand speed limits
+// The program first checks the cars speed and compares to the given standard limit
+// Then the program gives point deductions(demerits) for going above limit
 function speedCheck(speed)
 {
     const speedLimit = 70;
     const kmPerDemerit = 5;
+    //The standard speed limit and speed increase where demerits start
 
     if (speed <= speedLimit)
     {
         return "Ok"
     }
+    // checking speed limit
     else 
     {
         const demeritPoints = Math.floor ((speedLimit)/kmPerDemerit);
@@ -43,5 +50,51 @@ function speedCheck(speed)
         {
             return `Points : ${demeritPoints}`
         }
+    }
+    // computations for demerits to be given if above speed limit
 }
+
+// A function that accepts ones input of theirgross salary
+// Then the function calculates all deductions and taxes respectively and gives the expected net salary 
+// It uses a fixed personal relief;
+
+function calcSalary(personalRelief, netSalary,totalDeductions, contribitionBenefit,NHIFDeductions, NSSFDeductions, grossSalary, taxableIncome){
+    let paye;
+    personalRelief = +2400;
+    grossSalary = +prompt("Enter your salary amount:")
+    contribitionBenefit = +prompt("Contribution Benefit:")
+    NHIFDeductions= +prompt("Enter NHIFDeduction:")
+    NSSFDeductions= +prompt("Enter NSSFDeduction:")  
+
+    //checking the taxable income; 
+
+    
+    totalDeductions = (NHIFDeductions + NSSFDeductions + personalRelief + contribitionBenefit);
+    taxableIncome = grossSalary - totalDeductions;
+
+    console.log(`your taxableIncome is ksh: ${taxableIncome}`)
+    console.log(`your totalDeductions is ksh: ${totalDeductions}`)
+
+    //calculating monthly pay as you earn(PAYE) based on income per person; 
+
+    switch(true){
+        case (taxableIncome<24000):
+            paye = taxableIncome * 0.01;
+            break;
+
+        case (taxableIncome>=24001 && taxableIncome<= 32333):
+            paye = taxableIncome * 0.25;
+            break;
+
+        case (taxableIncome>32333):
+            paye = taxableIncome * 0.30;
+            break;  
+
+    }
+    // deductions made of PAYE and then the resulting netsalary; 
+    netSalary = (parseInt (taxableIncome - paye));
+    console.log(`your netSalary is ksh: ${netSalary}`)
+    
+
 }
+calcSalary()
